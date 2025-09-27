e3afcec3d7ecceeba3c327076eca3c9a3aff3fd5
Kevin Jordan <Kevinmauricejordan2005@gmail.com>
2025-09-27 20:27:16 +0200

    TOP

--- js/localstorage.js
+++ js/localstorage.js
@@ -6,11 +6,11 @@ const setToLocalstorage = (traderName, arr) => {
 const getItemsFromLocalstorage = (traderName) => {
 	const traderRef = JSON.parse(localStorage.getItem(traderName));
 	const list = traderCompleteQuestLists[traderName];
-	for (let i = 0; i < traderRef.length; i++) {
-		const item = traderRef[i];
-		list.push(item);
-	}
 	if (traderRef !== null) {
+		for (let i = 0; i < traderRef.length; i++) {
+			const item = traderRef[i];
+			list.push(item);
+		}
 		addClass(traderRef);
 	}
 };
